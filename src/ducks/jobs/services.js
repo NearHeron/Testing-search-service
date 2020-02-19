fcf33f7d3855c673c6dd23d0ac5227a3db855533
NearHeron <58370847+NearHeron@users.noreply.github.com>
2020-02-20 05:44:32 +0700

    Update services.js

--- src/ducks/jobs/services.js
+++ src/ducks/jobs/services.js
@@ -2,14 +2,14 @@ import request from "../helpers/request";
 
 export default {
   getJobs({description, location, full_time}) {
-    /*return request.get('/positions.json',{
+    return request.get('/positions.json',{
       params: {
         description: description,
         location: location,
         full_time: full_time,
       },
-    });*/
-    return request.get('planets/1/')
+    });
+/*    return request.get('planets/1/')*/
   }
 };
 
